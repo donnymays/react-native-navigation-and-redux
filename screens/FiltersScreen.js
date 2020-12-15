e922b9e608abc8fc0482e03ffae4e8ab55b05c73
donnymays <dtmays2@gmail.com>
2020-12-15 12:59:29 -0800

    add filter switches that toggle state for filter params

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/colors';
+
+const FilterSwitch = props => {
+  return(
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{true: Colors.primaryColor}}
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+        value={props.state} 
+        onValueChange={props.onChange} />
+    </View>
+  )
+}
+
 
 const FiltersScreen = props => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text> The FiltersScreen</Text>
+      <Text style={styles.title}>Avaliable Filters</Text>
+      <FilterSwitch label='Gluten Free' state={isGlutenFree} onChange={newValue => setIsGlutenFree(newValue)} />
+      <FilterSwitch label='Lactose Free' state={isLactoseFree} onChange={newValue => setIsLactoseFree(newValue)} />
+      <FilterSwitch label='Vegan' state={isVegan} onChange={newValue => setIsVegan(newValue)} />
+      <FilterSwitch label='Vegetarian' state={isVegetarian} onChange={newValue => setIsVegetarian(newValue)} />
     </View>
   );
 };
@@ -20,13 +44,34 @@ FiltersScreen.navigationOptions = (navData) => {
           navData.navigation.toggleDrawer();
         }} />
       </HeaderButtons>
-    )
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Menu' iconName='ios-save' onPress={() => {
+          navData.navigation.toggleDrawer();
+        }} />
+      </HeaderButtons>
+    )  
   }
 }
 
 const styles = StyleSheet.create({
   screen : {
-    flex: 1
+    flex: 1,
+    alignItems: 'center'
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 15
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center'
   }
 });
 
