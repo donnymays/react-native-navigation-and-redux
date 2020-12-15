311ab569680172aa2ebad7845771b7ffd46b9874
donnymays <dtmays2@gmail.com>
2020-12-15 14:41:00 -0800

    install redux and create mealReducer

--- App.js
+++ App.js
@@ -4,10 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
-
+import { createStore, combineReducers } from 'redux';
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
+import { Provider } from 'react-redux';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   Font.loadAsync({
@@ -28,7 +36,9 @@ export default function App() {
   }
   
   return (
-    <MealsNavigator />
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
 
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, ScrollView, Image, TextComponent } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { MEALS } from '../data/dummy-data';
+import { useSelector} from 'react-redux';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -14,8 +14,9 @@ const ListItem = props => {
 }
 
 const MealDetailsScreen = props => {
+  const availableMeals = useSelector(state => state.meals.meals)
   const mealId = props.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
   return (
     <ScrollView>
@@ -35,9 +36,14 @@ const MealDetailsScreen = props => {
 
 MealDetailsScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+  
+  // useEffect(() => {
+  //   props.navigation.setParams({mealTitle: selectedMeal.title});
+  // }, [selectedMeal])
+
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: (     
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item 
