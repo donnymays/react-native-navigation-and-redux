0f8aab2b56dccb52df0de282a107f9af121a8d11
donnymays <dtmays2@gmail.com>
2020-12-15 15:19:54 -0800

    clicking star icon adds meals to favorites list/screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, Text} from 'react-native';
 import MealList from '../components/MealList';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
 
 const FavoritesScreen = props => {
-  const favMeals = useSelector(state => state.meals.favoriteMeals)
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
   
   return (
     <MealList listData={favMeals} navigation={props.navigation} />
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,55 +1,69 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import React, { useEffect, useCallback } from 'react';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
+import { toggleFavorite } from '../store/actions/meals';
 
 const ListItem = props => {
   return (
     <View style={styles.listItem}>
       <DefaultText>{props.children}</DefaultText>
     </View>
-  )
-}
+  );
+};
 
-const MealDetailsScreen = props => {
-  const availableMeals = useSelector(state => state.meals.meals)
+const MealDetailScreen = props => {
+  const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
+
   const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  const dispatch = useDispatch();
+
+  const toggleFavoriteHandler = useCallback(() => {
+    dispatch(toggleFavorite(mealId));
+  }, [dispatch, mealId]);
+
+  useEffect(() => {
+    props.navigation.setParams({toggleFav: toggleFavoriteHandler});
+  }, [toggleFavoriteHandler]);
 
   return (
     <ScrollView>
-      <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
       <View style={styles.details}>
         <DefaultText>{selectedMeal.duration}m</DefaultText>
         <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
         <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
       </View>
       <Text style={styles.title}>Ingredients</Text>
-      {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+      {selectedMeal.ingredients.map(ingredient => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
       <Text style={styles.title}>Steps</Text>
-      {selectedMeal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
+      {selectedMeal.steps.map(step => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
     </ScrollView>
   );
 };
 
-MealDetailsScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam('mealId');
+MealDetailScreen.navigationOptions = navigationData => {
+  // const mealId = navigationData.navigation.getParam('mealId');
   const mealTitle = navigationData.navigation.getParam('mealTitle');
-  
-  // useEffect(() => {
-  //   props.navigation.setParams({mealTitle: selectedMeal.title});
-  // }, [selectedMeal])
-
+  const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  // const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
-    headerRight: (     
+    headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item 
-          title='Favorite' 
-          iconName='ios-star' 
-          onPress={() => {console.log('Favorited!');}} 
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={toggleFavorite}
         />
       </HeaderButtons>
     )
@@ -77,10 +91,7 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderWidth: 1,
     padding: 10
-
   }
-
-
 });
 
-export default MealDetailsScreen;
+export default MealDetailScreen;
