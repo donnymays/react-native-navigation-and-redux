b956d1947c62528b1229afe0f8b99a7466cebf04
donnymays <dtmays2@gmail.com>
2020-12-15 15:57:49 -0800

    toggle favorite meal icon in header and create text for favorites screen if no favorite meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return(
+      <View style={styles.content}>
+        <DefaultText>No Favorites Selected</DefaultText>
+      </View>
+    )
+  }
   
   return (
     <MealList listData={favMeals} navigation={props.navigation} />
@@ -24,5 +34,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavoritesScreen;
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useCallback } from 'react';
-import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet, NativeAppEventEmitter } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -18,7 +18,9 @@ const ListItem = props => {
 const MealDetailScreen = props => {
   const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
-
+  const currentMealIsFavorite = useSelector(state => 
+    state.meals.favoriteMeals.some(meal => meal.id === mealId)
+  );
   const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
   const dispatch = useDispatch();
@@ -30,6 +32,11 @@ const MealDetailScreen = props => {
   useEffect(() => {
     props.navigation.setParams({toggleFav: toggleFavoriteHandler});
   }, [toggleFavoriteHandler]);
+
+
+  useEffect(() => {
+    props.navigation.setParams({isFav: currentMealIsFavorite});
+  }, [currentMealIsFavorite]);
 
   return (
     <ScrollView>
@@ -55,6 +62,7 @@ MealDetailScreen.navigationOptions = navigationData => {
   // const mealId = navigationData.navigation.getParam('mealId');
   const mealTitle = navigationData.navigation.getParam('mealTitle');
   const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  const isFavorite = navigationData.navigation.getParam('isFav');
   // const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
@@ -62,7 +70,7 @@ MealDetailScreen.navigationOptions = navigationData => {
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Favorite"
-          iconName="ios-star"
+          iconName={isFavorite ? "ios-star" : "ios-star-outline"} 
           onPress={toggleFavorite}
         />
       </HeaderButtons>
